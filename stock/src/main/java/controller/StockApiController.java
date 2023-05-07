bac21d6e8805ca26c6adcdd5c4d24783d56a31a2
xxr <1587321843@qq.com>
2023-05-07 23:58:37 +0200

    finish the basic functions for stock database

--- stock/src/main/java/controller/StockApiController.java
+++ stock/src/main/java/controller/StockApiController.java
@@ -1,23 +1,44 @@
 package controller;
 
-import java.util.Map;
+import com.datastax.driver.core.DataType;
+import com.datastax.driver.core.Row;
 
+import java.util.Map;
+import java.util.UUID;
+
+import services.StockService;
 public class StockApiController {
+    private static final StockService stock_service=new StockService();
     public Map<String, Object> find(String itemId) {
-        Map<String, Object> res = Map.of("stock", 10, "price", 100);
-        return res;
+        UUID uuid = UUID.fromString(itemId);
+        Row row= stock_service.findItemByID(uuid);
+//      return Map.of("stock", 10, "price", 100);
+        return Map.of("item_id", row.getUUID("item_id"), "stock", row.getInt("stock"), "price", row.getInt("price"));
     }
 
     public String subtract(String itemId, int amount) {
-        return "Success";
+        UUID id = UUID.fromString(itemId);
+        Row row = stock_service.SubStock(id,amount);
+        Map<String, String> resultMap = Map.of(
+                "item_id", row.getUUID("item_id").toString(),
+                "stock", Integer.toString(row.getInt("stock")),
+                "price", Integer.toString(row.getInt("price")));
+        return resultMap.toString();
     }
 
     public String add(String itemId, int amount) {
-        return "Success";
+        UUID id = UUID.fromString(itemId);
+        Row row = stock_service.AddStock(id,amount);
+        Map<String, String> resultMap = Map.of(
+                "item_id", row.getUUID("item_id").toString(),
+                "stock", Integer.toString(row.getInt("stock")),
+                "price", Integer.toString(row.getInt("price")));
+        return resultMap.toString();
     }
 
     public Map<String, Object> createItem(int price) {
-        Map<String, Object> res = Map.of("item_id", 123);
+        UUID itemID=stock_service.CreateItem(price);
+        Map<String, Object> res = Map.of("item_id", itemID,"price:",price,"stock:",0);
         return res;
     }
 }
--- stock/src/main/java/services/StockService.java
+++ stock/src/main/java/services/StockService.java
@@ -1,14 +1,38 @@
 package services;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
+import com.datastax.driver.core.*;
 import com.datastax.driver.mapping.MappingManager;
+import models.Item;
+
+import java.util.UUID;
+
+import static com.datastax.driver.core.DataType.uuid;
 
 public class StockService {
     static Cluster cluster = null;
     static Session session = null;
     static MappingManager mapper = null;
     static boolean initialized = false;
+
+    public static void createKeyspace(String keyspace) {
+        String query = "CREATE KEYSPACE IF NOT EXISTS " + keyspace
+                + " WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};";
+        session.execute(query);
+    }
+    public static void useKeyspace(String keyspace) {
+        session.execute("USE " + keyspace);
+    }
+
+    public static void createTable(String tableName) {
+        String query = "CREATE TABLE IF NOT EXISTS " + tableName + " ("
+                + "item_id uuid PRIMARY KEY,"
+                + "stock int,"
+                + "price int);";
+        session.execute(query);
+        query = "INSERT INTO items (item_id, stock, price) VALUES (9552eace-06a7-4a5e-a90d-9200063ed94a,100,6)";
+        session.execute(query);
+
+    }
 
     public static void init() throws InterruptedException {
         System.out.println("Initializing connection to Cassandra...");
@@ -18,6 +42,11 @@ public class StockService {
                         .addContactPoint("stock-db")
                         .build();
                 session = cluster.connect();
+                createKeyspace("stock_keyspace");
+                useKeyspace("stock_keyspace");
+                createTable("items");
+//                Item newItem = new Item("9552eace-06a7-4a5e-a90d-9200063ed94a", 10, 6);
+//                insertItem(newItem);
                 mapper = new MappingManager(session);
                 initialized = true;
             } catch (Exception e) {
@@ -25,8 +54,54 @@ public class StockService {
                 Thread.sleep(5000);
             }
         }
-        System.out.println("Connection to Cassandra initialized.");
+        System.out.println("Connection to Cassandra_stock initialized.");
     }
 
     // Here implement functions to interact with Cassandra
+    public Row findItemByID(UUID itemID){
+        session.execute("USE stock_keyspace");
+//        String m2="SELECT * FROM items WHERE item_id = "+itemID;
+//        System.out.println(m2);
+//        ResultSet resultSet = session.execute("SELECT * FROM items WHERE item_id = ?", itemID);
+        String query = "SELECT * FROM items WHERE item_id = ?";
+        PreparedStatement preparedStatement = session.prepare(query);
+        BoundStatement boundStatement = preparedStatement.bind(itemID);
+        ResultSet resultSet=session.execute(boundStatement);
+//        System.out.println(m2);
+        System.out.println("Item ID data type: " + itemID.getClass().getName());
+        return resultSet.one();
+    }
+    public UUID CreateItem(int price) {
+//        PreparedStatement preparedStatement = session.prepare(query);
+//         Bind the values to the query
+//        BoundStatement boundStatement = preparedStatement.bind(id, 0, price);
+//        session.execute(boundStatement);
+        UUID id= UUID.randomUUID();
+        System.out.println(id);
+        String query = "INSERT INTO items (item_id, stock, price) VALUES ("+id+",0,"+price+")";
+        System.out.println(query);
+        session.execute(query);
+
+        return id;
+    }
+    public Row AddStock(UUID itemID,int amount){
+        Row row = findItemByID(itemID);
+        int original_stock = row.getInt("stock");
+        int new_stock = original_stock+amount;
+        String query = "UPDATE items SET stock = ? WHERE item_id = ?";
+        PreparedStatement preparedStatement = session.prepare(query);
+        BoundStatement boundStatement = preparedStatement.bind(new_stock, itemID);
+        session.execute(boundStatement);
+        return findItemByID(itemID);
+    }
+    public Row SubStock(UUID itemID,int amount){
+        Row row = findItemByID(itemID);
+        int original_stock = row.getInt("stock");
+        int new_stock = original_stock-amount;
+        String query = "UPDATE items SET stock = ? WHERE item_id = ?";
+        PreparedStatement preparedStatement = session.prepare(query);
+        BoundStatement boundStatement = preparedStatement.bind(new_stock, itemID);
+        session.execute(boundStatement);
+        return findItemByID(itemID);
+    }
 }
