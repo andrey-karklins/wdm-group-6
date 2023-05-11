951a9fcb8ffe1fb3f0df8a02ccccb1a455a951c4
hujjle <45731880+hujjle@users.noreply.github.com>
2023-05-11 16:05:51 +0200

    some code for order

--- order/src/main/java/app/controllers/OrderApiController.java
+++ order/src/main/java/app/controllers/OrderApiController.java
@@ -1,38 +1,66 @@
 package app.controllers;
 
+import app.models.Order;
 import app.services.OrderService;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class OrderApiController {
+//    private OrderService orderService;
+    private static final OrderService orderService = new OrderService();
+
+//    public OrderApiController(OrderService orderService) {
+//        this.orderService = orderService;
+//    }
+
     public Map<String, Object> createOrder(String userId) {
-        Map<String, Object> res = Map.of("order_id", 123);
-        return res;
+        Order order = new Order();
+        order.user_id = UUID.fromString(userId);
+        order = orderService.createOrder(order);
+        return Map.of("order_id", order.order_id);
     }
 
     public String cancelOrder(String orderId) {
+        UUID orderUUID = UUID.fromString(orderId);
+        orderService.deleteOrder(orderUUID);
         return "Success";
     }
 
     public Map<String, Object> findOrder(String orderId) {
-        Map<String, Object> res = Map.of("order_id", orderId,
-                "paid", true,
-                "items", List.of("123", "456"),
-                "user_id", "123",
-                "total_cost", 100);
-        return res;
+        UUID orderUUID = UUID.fromString(orderId);
+        Order order = orderService.findOrder(orderUUID);
+        if (order != null) {
+            return Map.of("order_id", order.order_id,
+                    "paid", order.paid,
+                    "items", order.items,
+                    "user_id", order.user_id,
+                    "total_cost", order.total_cost);
+        } else {
+            return Map.of("error", "Order not found");
+        }
     }
 
     public String addItem(String orderId, String itemId) {
+        UUID orderUUID = UUID.fromString(orderId);
+        UUID itemUUID = UUID.fromString(itemId);
+        orderService.addItemToOrder(orderUUID, itemUUID);
         return "Success";
     }
 
     public String removeItem(String orderId, String itemId) {
+        UUID orderUUID = UUID.fromString(orderId);
+        UUID itemUUID = UUID.fromString(itemId);
+        orderService.removeItemFromOrder(orderUUID, itemUUID);
         return "Success";
     }
 
     public String checkout(String orderId) {
-        return "Success";
+        UUID orderUUID = UUID.fromString(orderId);
+        boolean result = orderService.checkoutOrder(orderUUID);
+        if (result) {
+            return "Success";
+        } else {
+            return "Failed";
+        }
     }
 }
--- order/src/main/java/app/services/OrderService.java
+++ order/src/main/java/app/services/OrderService.java
@@ -1,9 +1,13 @@
 package app.services;
 
 import app.OrderApp;
+import app.models.Order;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.mapping.Mapper;
 import com.datastax.driver.mapping.MappingManager;
+
+import java.util.UUID;
 
 public class OrderService {
     static Cluster cluster = null;
@@ -19,6 +23,9 @@ public class OrderService {
                         .addContactPoint("order-db")
                         .build();
                 session = cluster.connect();
+                createKeyspace("order_keyspace");
+                useKeyspace("order_keyspace");
+                createTable("orders");
                 mapper = new MappingManager(session);
                 initialized = true;
             } catch (Exception e) {
@@ -29,9 +36,78 @@ public class OrderService {
         System.out.println("Connection to Cassandra initialized.");
     }
 
+    // other methods...
+
+    public static void createKeyspace(String keyspace) {
+        String query = "CREATE KEYSPACE IF NOT EXISTS " + keyspace + " WITH replication "
+                + "= {'class':'SimpleStrategy', 'replication_factor':3};";
+        session.execute(query);
+    }
+
+    public static void useKeyspace(String keyspace) {
+        String query = "USE " + keyspace + ";";
+        session.execute(query);
+    }
+
+    public static void createTable(String tableName) {
+        String query = "CREATE TABLE IF NOT EXISTS " + tableName + " ("
+                + "order_id UUID PRIMARY KEY,"
+                + "user_id UUID,"
+                + "paid boolean,"
+                + "total_cost int,"
+                + "items list<UUID>"
+                + ");";
+        session.execute(query);
+    }
+
+
     public static void sendEvent(String event, String data) {
         OrderApp.clients.forEach(client -> client.sendEvent(event, data));
     }
 
     // Here implement functions to interact with Cassandra
+    public Order createOrder(Order order) {
+        Mapper<Order> orderMapper = mapper.mapper(Order.class);
+        orderMapper.save(order);
+        return order;
+    }
+
+    public void deleteOrder(UUID orderId) {
+        Mapper<Order> orderMapper = mapper.mapper(Order.class);
+        orderMapper.delete(orderId);
+    }
+
+    public Order findOrder(UUID orderId) {
+        Mapper<Order> orderMapper = mapper.mapper(Order.class);
+        return orderMapper.get(orderId);
+    }
+
+    public void addItemToOrder(UUID orderId, UUID itemId) {
+        Mapper<Order> orderMapper = mapper.mapper(Order.class);
+        Order order = orderMapper.get(orderId);
+        if (order != null) {
+            order.items.add(itemId);
+            orderMapper.save(order);
+        }
+    }
+
+    public void removeItemFromOrder(UUID orderId, UUID itemId) {
+        Mapper<Order> orderMapper = mapper.mapper(Order.class);
+        Order order = orderMapper.get(orderId);
+        if (order != null) {
+            order.items.remove(itemId);
+            orderMapper.save(order);
+        }
+    }
+
+    public boolean checkoutOrder(UUID orderId) {
+        Mapper<Order> orderMapper = mapper.mapper(Order.class);
+        Order order = orderMapper.get(orderId);
+        if (order != null && !order.paid) {
+            order.paid = true;
+            orderMapper.save(order);
+            return true;
+        }
+        return false;
+    }
 }
