698f43775f5fc25f6738e32138e9399dbf7ead5a
AlexDumitru17 <a.dumitru@student.tudelft.nl>
2023-05-12 23:19:18 +0200

    Add mapper implementation for order service

--- order/src/main/java/app/services/OrderService.java
+++ order/src/main/java/app/services/OrderService.java
@@ -3,17 +3,15 @@ package app.services;
 import app.OrderApp;
 import app.models.Order;
 import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.mapping.MappingManager;
 import java.util.ArrayList;
 import java.util.UUID;
-import java.util.List;
-
-import com.datastax.driver.core.Row;
-import com.datastax.driver.core.ResultSet;
 
 import com.datastax.driver.mapping.*;
+import com.datastax.driver.mapping.annotations.Accessor;
+import com.datastax.driver.mapping.annotations.Param;
+import com.datastax.driver.mapping.annotations.Query;
 
 
 public class OrderService {
@@ -22,12 +20,7 @@ public class OrderService {
     static MappingManager mapper = null;
     static boolean initialized = false;
 
-    private static PreparedStatement insertOrderStmt;
-    private static PreparedStatement deleteOrderStmt;
-    private static PreparedStatement findOrderStmt;
-    private static PreparedStatement addItemStmt;
-    private static PreparedStatement removeItemStmt;
-    private static PreparedStatement checkoutStmt;
+    private static String keyspace = "order_keyspace";
 
     static Mapper<Order> orderMapper;
 
@@ -39,21 +32,12 @@ public class OrderService {
                         .addContactPoint("order-db")
                         .build();
                 session = cluster.connect();
-                createKeyspace("order_keyspace");
-                useKeyspace("order_keyspace");
+                createKeyspace(keyspace);
+                useKeyspace(keyspace);
                 createTable("orders");
 
                 mapper = new MappingManager(session);
-                orderMapper = mapper.mapper(Order.class);
-
-                // Prepared statements initialization
-                insertOrderStmt = session.prepare("INSERT INTO orders (order_id, user_id, items, total_cost, paid) VALUES (?, ?, ?, ?, ?)");
-                deleteOrderStmt = session.prepare("DELETE FROM orders WHERE order_id = ?");
-                findOrderStmt = session.prepare("SELECT * FROM orders WHERE order_id = ?");
-                addItemStmt = session.prepare("UPDATE orders SET items = items + ?, total_cost = total_cost + ? WHERE order_id = ?");
-                removeItemStmt = session.prepare("UPDATE orders SET items = items - ?, total_cost = total_cost - ? WHERE order_id = ?");
-                checkoutStmt = session.prepare("UPDATE orders SET paid = true WHERE order_id = ?");
-
+                orderMapper = mapper.mapper(Order.class, keyspace);
                 initialized = true;
             } catch (Exception e) {
                 System.out.println("Cassandra is not ready yet, retrying in 5 seconds...");
@@ -62,8 +46,7 @@ public class OrderService {
         }
         System.out.println("Connection to Cassandra initialized.");
     }
-///
-    //////
+
     public static void createKeyspace(String keyspace) {
         String query = "CREATE KEYSPACE IF NOT EXISTS " + keyspace
                 + " WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};";
@@ -83,45 +66,47 @@ public class OrderService {
                 + "total_cost int);";
         session.execute(query);
     }
-///////
-    /////
 
     public static void sendEvent(String event, String data) {
         OrderApp.clients.forEach(client -> client.sendEvent(event, data));
     }
-////
-    ///
-    // Here implement functions to interact with Cassandra
+
 
     public UUID createOrder(UUID userId) {
         UUID orderId = UUID.randomUUID();
-        session.execute(insertOrderStmt.bind(orderId, userId, new ArrayList<UUID>(), 0, false));
+        Order order = new Order(orderId, userId, false, 0, new ArrayList<UUID>());
+        order.order_id = orderId;
+        orderMapper.save(order);
         return orderId;
     }
 
     public boolean cancelOrder(UUID orderId) {
-        session.execute(deleteOrderStmt.bind(orderId));
+        OrderAccessor orderAccessor = mapper.createAccessor(OrderAccessor.class);
+        Order order =  orderAccessor.getOrderById(orderId);
+        orderMapper.delete(order);
         return true;
     }
 
-
-
+    //TODO Retrieve the price from the stock microservice
     public boolean addItemToOrder(UUID orderId, UUID itemId) {
+
         int itemPrice = 1;//dummy
-        Order order = orderMapper.get(orderId);
+        Order order =  findOrderById(orderId);
         if (order == null) {
             return false;
         }
+
         order.items.add(itemId);
         order.total_cost += itemPrice;
-        System.out.println("Hello, World!");
         orderMapper.save(order);
         return true;
     }
 
+
+    //TODO Retrieve the price from the stock microservice
     public boolean removeItemFromOrder(UUID orderId, UUID itemId) {
         int itemPrice = 1;//dummy
-        Order order = orderMapper.get(orderId);
+        Order order =  findOrderById(orderId);
         if (order == null) {
             return false;
         }
@@ -131,35 +116,38 @@ public class OrderService {
         return true;
     }
 
-    public boolean checkoutOrder(UUID orderId) {
-        Order order = orderMapper.get(orderId);
-        if (order == null) {
-            return false;
-        }
-        order.paid = true;
-        orderMapper.save(order);
-        return true;
+
+    /**
+     * Finds and returns an order object given an unique orderId
+     * @param orderId the order id
+     * @return the order object from the database
+     */
+    public static Order findOrderById(UUID orderId) {
+        OrderAccessor orderAccessor = mapper.createAccessor(OrderAccessor.class);
+        return orderAccessor.getOrderById(orderId);
     }
 
-//    public boolean addItemToOrder(UUID orderId, UUID itemId) {
-//        int price = 1;//dummy
-//        session.execute(addItemStmt.bind(List.of(itemId), price, orderId));
-//        return true;
-//    }
-//
-//    public boolean removeItemFromOrder(UUID orderId, UUID itemId) {
-//        int price = 1;//dummy
-//        session.execute(removeItemStmt.bind(List.of(itemId), price, orderId));
-//        return true;
-//    }
-//
-//    public boolean checkoutOrder(UUID orderId) {
-//        session.execute(checkoutStmt.bind(orderId));
-//        return true;
-//    }
+    /**
+     * Changes the status of the order
+     * @param orderId the order id
+     * @return true if the change went through
+     */
+    public static boolean changePaidStatus(UUID orderId) {
+        boolean status = false;
+        Order order =  findOrderById(orderId);
 
-    public Row findOrder(UUID orderId) {
-        ResultSet rs = session.execute(findOrderStmt.bind(orderId));
-        return rs.one();
+        if (order != null) {
+            order.paid = !order.paid;
+            orderMapper.save(order);
+            status = true;
+
+        }
+        return status;
+    }
+
+    @Accessor
+    public interface OrderAccessor {
+        @Query("SELECT * FROM orders WHERE order_id = :order_id")
+        Order getOrderById(@Param("order_id") UUID order_id);
     }
 }
