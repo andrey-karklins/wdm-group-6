95a9efe4d6b3d39036fb843beedde8b216d7fa43
xxr <1587321843@qq.com>
2023-05-14 17:01:08 +0200

    renew the format

--- stock/src/main/java/app/services/OrderEventsService.java
+++ stock/src/main/java/app/services/OrderEventsService.java
@@ -9,18 +9,6 @@ import com.datastax.driver.core.*;
 import java.util.concurrent.TimeUnit;
 import app.services.StockService;
 public class OrderEventsService {
-    private static boolean connected = false;
-    public static void listen() throws InterruptedException {
-        Client client = ClientBuilder.newBuilder().build();
-        WebTarget target = client.target("http://order-service:5000/sse");
-        SseEventSource sseEventSource = SseEventSource.target(target).reconnectingEvery(1, TimeUnit.SECONDS).build();
-        sseEventSource.register(event -> handler(event.getName(), event.readData(String.class)));
-        sseEventSource.open();
-        while(!connected) {
-            Thread.sleep(1000);
-        }
-    }
-
     private static void HandleAddItem(String data){
         String[] parts = data.split(" ");
         String orderId = parts[0];
@@ -68,6 +56,19 @@ public class OrderEventsService {
         System.out.println(price);
         StockService.sendEvent("ItemStock",orderId+" "+itemId+" "+Integer.toString(price));
     }
+    private static boolean connected = false;
+    public static void listen() throws InterruptedException {
+        Client client = ClientBuilder.newBuilder().build();
+        WebTarget target = client.target("http://order-service:5000/sse");
+        SseEventSource sseEventSource = SseEventSource.target(target).reconnectingEvery(1, TimeUnit.SECONDS).build();
+        sseEventSource.register(event -> handler(event.getName(), event.readData(String.class)));
+        sseEventSource.open();
+        while(!connected) {
+            Thread.sleep(1000);
+        }
+    }
+
+
     private static void handler(String event, String data) {
         switch (event) {
             case "connected":
@@ -78,35 +79,11 @@ public class OrderEventsService {
             // ... (TODO)
             case "ifItemExists":
 //OrderService.sendEvent("ifItemExists",orderId+" "+itemId);
-                String[] parts = data.split(" ");
-                String orderId = parts[0];
-                String itemId = parts[1];
-                UUID order_id = UUID.fromString(orderId);
-                UUID item_id=UUID.fromString(itemId);
-                StockService stockService = new StockService();
-                Row res = stockService.findItemByID(item_id);
-                int price=0;
-                //cant find item
-                if( res ==  null){
-                    price=-2;
-                }
-                //item stock is zero
-                if (res!=null && res.getInt("stock")==0)
-                {
-                    price=-1;
-                }
-                //there is enough stock of item
-                if (res!=null && res.getInt("stock")>=1){
-                    price = res.getInt("price");
-                    Row sub = stockService.SubStock(item_id,1);
-                }
-                System.out.println(price);
-                StockService.sendEvent("ItemStock",orderId+" "+itemId+" "+Integer.toString(price));
+                HandleAddItem(data);
                 break;
             case "ItemRemoval":
-                HandleRemoveItem(data);
+                HandleRemoveItem(data0);
                 break;
-
             default:
                 System.out.println("Unknown event: " + event);
         }
