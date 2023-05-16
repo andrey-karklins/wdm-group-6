0a290c293feaa33f3becffc5f873c173f39088e4
Andrey Karklins <andrey.karklins@gmail.com>
2023-05-16 14:03:07 +0200

    Fix k8 scripts

--- deploy-charts-cluster.sh
+++ deploy-charts-cluster.sh
@@ -5,5 +5,7 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 
 helm repo update
 
-helm install -f helm-config/cassandra-helm-values.yaml cassandra bitnami/cassandra
+helm install -f helm-config/order-db-values.yaml order-db bitnami/cassandra
+helm install -f helm-config/stock-db-values.yaml stock-db bitnami/cassandra
+helm install -f helm-config/payment-db-values.yaml payment-db bitnami/cassandra
 helm install -f helm-config/nginx-helm-values.yaml nginx ingress-nginx/ingress-nginx
