919445aa2f10dd7ce8b9c0de5ffb9ce38933044a
Andrey Karklins <andrey.karklins@gmail.com>
2023-05-16 14:00:47 +0200

    Fix k8 scripts

--- deploy-charts-minikube.sh
+++ deploy-charts-minikube.sh
@@ -3,4 +3,6 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
+helm install -f helm-config/order-db-values.yaml order-db bitnami/cassandra
+helm install -f helm-config/stock-db-values.yaml stock-db bitnami/cassandra
+helm install -f helm-config/payment-db-values.yaml payment-db bitnami/cassandra
